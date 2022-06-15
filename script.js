acb9f00ac147be43133efe75d74e56b4f7e41b87
gerlangiosoares <gerlangio@gmail.com>
2022-06-15 14:33:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ function contar() {
     var ini = window.document.getElementById('txti')
     var fim = window.document.getElementById('txtf')
     var passo = window.document.getElementById('txtp')
+    var res = window.document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
         window.alert('[ERRO] Faltam dados!')
     } else {
-        window.alert('Tudo OK!')
+        //window.alert('Tudo OK!')
+        res.innerHTML = 'Contando: '
+        //Convertendo txt para numero
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
     }
 }
