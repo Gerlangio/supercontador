97f796db855f4032d0a7db5064332915ac3d1a11
gerlangiosoares <gerlangio@gmail.com>
2022-06-15 15:37:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,31 @@ function contar() {
     var res = window.document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Imposível contar!'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
         //window.alert('Tudo OK!')
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         //Convertendo txt para numero
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //Contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem decrescente
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
