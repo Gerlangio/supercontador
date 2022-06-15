af120bc5efb41f911427f601f68e901ba2fceb47
gerlangiosoares <gerlangio@gmail.com>
2022-06-15 14:23:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function contar() {
     //o let substitui o var
-    var ini = window.document('txti')
-    var fim = window.document('txtf')
-    var passo = window.document('txtp')
+    var ini = window.document.getElementById('txti')
+    var fim = window.document.getElementById('txtf')
+    var passo = window.document.getElementById('txtp')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        window.alert('Tudo OK!')
+    }
 }
